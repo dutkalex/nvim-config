69f305c16f74eb197ad0275a87c221ea69a70f15
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2026-02-24 23:41:23 +0100

    cleanup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,14 @@
 return {
-  'nvim-telescope/telescope.nvim', 
+  'nvim-telescope/telescope.nvim',
   tag = 'v0.2.1',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    -- 'jonarrien/telescope-cmdline.nvim',
   },
-  -- keys = {
-  --   { ':', '<cmd>Telescope cmdline<cr>', desc = 'Command' }
-  -- },
   config = function()
     require('telescope').setup {
       extensions = { fzf = {} }
     }
     require('telescope').load_extension('fzf')
-    -- require("telescope").load_extension('cmdline')
   end
 }
- 

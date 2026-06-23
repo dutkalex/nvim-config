23d387ace44c0cd751174a6c5060cddf32672769
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2026-06-23 20:52:45 +0200

    extract new files

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require("config.options")
 require("config.lazy")
 
 -- Treesitter setup
@@ -54,30 +55,6 @@ vim.lsp.enable({'ruff'})
 
 vim.diagnostic.config({
   virtual_lines = { current_line = true },
-})
-
-vim.opt.number = true             -- line numbers
-vim.opt.scrolloff = 8             -- keep at least 8 lines between the cursor and the top/bottom
-vim.opt.signcolumn = "yes"        -- always display sign column
-vim.opt.clipboard = "unnamedplus" -- Use system clipboard
-vim.opt.expandtab = true          -- insert spaces instead of tabs
-vim.opt.inccommand = "split"      -- show find-replaces live
--- vim.opt.smartcase = true
--- vim.opt.ignorecase = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "lua" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.softtabstop = 2
-  end,
 })
 
 -- Colorscheme
---
+++ lua/config/options.lua
@@ -0,0 +1,15 @@
+vim.opt.number = true             -- line numbers
+vim.opt.scrolloff = 8             -- keep at least 8 lines between the cursor and the top/bottom
+vim.opt.signcolumn = "yes"        -- always display sign column
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+vim.opt.expandtab = true          -- insert spaces instead of tabs
+vim.opt.inccommand = "split"      -- show find-replaces live
+-- vim.opt.smartcase = true
+-- vim.opt.ignorecase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+

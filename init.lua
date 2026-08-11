1e01b10ee78c15404ba02a236b66bcfb6b4b34e2
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2026-08-11 22:33:20 +0200

    cleanup

--- init.lua
+++ init.lua
@@ -31,11 +31,7 @@ vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { link = "Comment" }) -- Color i
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
--- Find commands
-local telescope_builtin = require('telescope.builtin')
-local custom_finds = require("config.custom-finds")
-
-local disable_if_oil_buffer = function(fn)
+local if_not_oil = function(fn)
   return function()
     if vim.bo.filetype == "oil" then
       return
@@ -44,18 +40,40 @@ local disable_if_oil_buffer = function(fn)
   end
 end
 
+-- Find commands (leader f)
+local telescope_builtin = require('telescope.builtin')
+local custom_finds = require("config.custom-finds")
 vim.keymap.set("n", "<leader>ff", custom_finds.find_files, { desc = "[F]ind [F]ile" })
 vim.keymap.set("n", "<leader>frf", telescope_builtin.oldfiles, { desc = "[F]ind [R]ecent [F]ile" })
 vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "[F]ind [B]uffer" })
 vim.keymap.set("n", "<leader>fnf", custom_finds.find_neovim_config_files, { desc = "[F]ind [N]eovim configuration [F]ile" })
-vim.keymap.set("n", "<leader>fc", disable_if_oil_buffer(custom_finds.find_code), { desc = "[F]ind [C]ode" })
-vim.keymap.set("n", "<leader>fd", disable_if_oil_buffer(custom_finds.find_definitions), { desc = "[F]ind [D]efinitions" })
-vim.keymap.set("n", "<leader>fr", disable_if_oil_buffer(custom_finds.find_references), { desc = "[F]ind [R]eferences" })
-vim.keymap.set("n", "<leader>fh", disable_if_oil_buffer(telescope_builtin.help_tags), { desc = "[F]ind [H]elp" })
-vim.keymap.set("n", "<leader>fk", disable_if_oil_buffer(telescope_builtin.keymaps), { desc = "[F]ind [K]eymap" })
+vim.keymap.set("n", "<leader>fc", if_not_oil(custom_finds.find_code), { desc = "[F]ind [C]ode" })
+vim.keymap.set("n", "<leader>fd", if_not_oil(custom_finds.find_definitions), { desc = "[F]ind [D]efinitions" })
+vim.keymap.set("n", "<leader>fr", if_not_oil(custom_finds.find_references), { desc = "[F]ind [R]eferences" })
+vim.keymap.set("n", "<leader>fh", if_not_oil(telescope_builtin.help_tags), { desc = "[F]ind [H]elp" })
+vim.keymap.set("n", "<leader>fk", if_not_oil(telescope_builtin.keymaps), { desc = "[F]ind [K]eymap" })
 
-vim.keymap.set("n", "<leader>er", disable_if_oil_buffer(vim.lsp.buf.rename), { desc = "[E]dit [R]ename" })
-vim.keymap.set("n", "<leader>ef", disable_if_oil_buffer(vim.lsp.buf.format), { desc = "[E]dit [F]ormat" })
+-- Edit commands (leader e)
+vim.keymap.set("n", "<leader>er", if_not_oil(vim.lsp.buf.rename), { desc = "[E]dit [R]ename" })
+vim.keymap.set("n", "<leader>ef", if_not_oil(vim.lsp.buf.format), { desc = "[E]dit [F]ormat" })
+
+-- Diagnostics commands (leader d)
+local diag = require("config.diagnostics")
+vim.keymap.set('n', '<leader>dd', if_not_oil(function() telescope_builtin.diagnostics({ bufnr = 0 }) end),
+{ desc = "List [D]ocument [D]iagnostics" })
+vim.keymap.set('n', '<leader>dn', if_not_oil(diag.next), { desc = "[D]iagnostic [N]ext" })
+vim.keymap.set('n', '<leader>dp', if_not_oil(diag.prev), { desc = "[D]iagnostic [P]revious" })
+vim.keymap.set("n", "<leader>dl", if_not_oil(diag.less), { desc = "[D]iagnostics show [L]ess" })
+vim.keymap.set("n", "<leader>dm", if_not_oil(diag.more), { desc = "[D]iagnostics show [M]ore" })
+vim.keymap.set("n", "<leader>ds", if_not_oil(diag.show), { desc = "[D]iagnostic [S]how" })
+
+-- Git commands (leader g)
+local gitsigns = require('gitsigns')
+vim.keymap.set("n", "<leader>gn", if_not_oil(gitsigns.next_hunk), { desc = "[G]it [N]ext" })
+vim.keymap.set("n", "<leader>gp", if_not_oil(gitsigns.prev_hunk), { desc = "[G]it [P]revious" })
+vim.keymap.set("n", "<leader>gd", if_not_oil(gitsigns.preview_hunk_inline), { desc = "[G]it [D]iff" })
+vim.keymap.set("n", "<leader>ga", if_not_oil(gitsigns.stage_hunk), { desc = "[G]it [A]dd" })
+vim.keymap.set("n", "<leader>gr", if_not_oil(gitsigns.reset_hunk), { desc = "[G]it [R]eset" })
 
 local format_selection = function()
   local start_pos = vim.api.nvim_buf_get_mark(0, "<")
@@ -69,32 +87,12 @@ local format_selection = function()
   })
 end
 
-vim.keymap.set("v", "<leader>ef", disable_if_oil_buffer(format_selection), { desc = "[E]dit [F]ormat" })
+vim.keymap.set("v", "<leader>ef", if_not_oil(format_selection), { desc = "[E]dit [F]ormat" })
 
 -- Terminal
 local terminal = require("config.terminal")
 vim.keymap.set("n", "<leader>mt", terminal.create_mini_terminal, { desc = "Open a new [M]ini [T]erminal" })
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>:q<CR>") -- double escape to close the mini terminal
-
-
--- Diagnostics
-local diag = require("config.diagnostics")
-vim.keymap.set('n', '<leader>dd', disable_if_oil_buffer(function() telescope_builtin.diagnostics({ bufnr = 0 }) end),
-  { desc = "List [D]ocument [D]iagnostics" })
-vim.keymap.set('n', '<leader>dn', disable_if_oil_buffer(diag.next), { desc = "[D]iagnostic [N]ext" })
-vim.keymap.set('n', '<leader>dp', disable_if_oil_buffer(diag.prev), { desc = "[D]iagnostic [P]revious" })
-vim.keymap.set("n", "<leader>dl", disable_if_oil_buffer(diag.less), { desc = "[D]iagnostics show [L]ess" })
-vim.keymap.set("n", "<leader>dm", disable_if_oil_buffer(diag.more), { desc = "[D]iagnostics show [M]ore" })
-vim.keymap.set("n", "<leader>ds", disable_if_oil_buffer(diag.show), { desc = "[D]iagnostic [S]how" })
-
--- Git
-local gitsigns = require('gitsigns')
-vim.keymap.set("n", "<leader>gn", disable_if_oil_buffer(gitsigns.next_hunk), { desc = "[G]it [N]ext" })
-vim.keymap.set("n", "<leader>gp", disable_if_oil_buffer(gitsigns.prev_hunk), { desc = "[G]it [P]revious" })
-vim.keymap.set("n", "<leader>gd", disable_if_oil_buffer(gitsigns.preview_hunk_inline), { desc = "[G]it [D]iff" })
-vim.keymap.set("n", "<leader>ga", disable_if_oil_buffer(gitsigns.stage_hunk), { desc = "[G]it [A]dd" })
-vim.keymap.set("n", "<leader>gr", disable_if_oil_buffer(gitsigns.reset_hunk), { desc = "[G]it [R]eset" })
-
 
 local close_floating_windows = function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do

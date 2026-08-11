e9eb379e505c798020160f3cb07488286c7c11d5
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2026-08-11 22:49:16 +0200

    replace mini-indentscope -> indent-blankline

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -1,7 +1,19 @@
 return {
-  "nvim-mini/mini.indentscope",
-  config = {
-    symbol = '|',
-    animation = function(s, n) return 1 end,
-  }
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = {
+        char = "│",
+      },
+    },
+  },
 }
+--
+-- return {
+--   "nvim-mini/mini.indentscope",
+--   config = {
+--     symbol = '|',
+--     animation = function(s, n) return 1 end,
+--   }
+-- }

ca7f3d662a513c1ee947d339e978e1bc37226810
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2026-08-16 15:41:50 +0200

    disable blame on unsaved buffers

--- lua/config/git.lua
+++ lua/config/git.lua
@@ -68,47 +68,31 @@ local next_hunk = function(blame_output_lines, i)
   return hunk, i
 end
 
-local blame_hunks = function(bufnr)
+local ns = vim.api.nvim_create_namespace("git_blame")
+local blame_enabled = false
+
+local enable_blame = function(bufnr)
+  if blame_enabled then
+    vim.notify("Blame hunks already enabled", vim.log.levels.INFO)
+    return
+  end
+
+  if vim.bo[bufnr].modified then
+    vim.notify("Blame hunks can't be enabled, save buffer first", vim.log.levels.INFO)
+    return
+  end
+
   local blame_output_lines = raw_blame_output(bufnr)
   if not blame_output_lines then
     return nil
   end
 
-  local hunks = {}
   local i = 1
   local buffer_line = 0
 
   while i <= #blame_output_lines do
     local hunk, new_i = next_hunk(blame_output_lines, i)
-    table.insert(hunks, {
-      first = buffer_line,
-      last = buffer_line + hunk.line_count - 1,
-      sha = hunk.commit,
-      author = hunk.author or hunk.commit:sub(1, 8),
-    })
-    buffer_line = buffer_line + hunk.line_count
-    i = new_i
-  end
 
-  return hunks
-end
-
-local ns = vim.api.nvim_create_namespace("git_blame")
-local blame_enabled = false
-
-local show_blame_view = function(bufnr)
-  if blame_enabled then
-    vim.notify("Blame hunks already activated", vim.log.levels.INFO)
-    return
-  end
-
-  local hunks = blame_hunks(bufnr)
-  if not hunks then
-    vim.notify("Blame hunks reconstruction failed", vim.log.levels.INFO)
-    return
-  end
-
-  for _, hunk in ipairs(hunks) do
     local mark = function(line, text)
       vim.api.nvim_buf_set_extmark(bufnr, ns, line, 0, {
         virt_text = {
@@ -120,38 +104,52 @@ local show_blame_view = function(bufnr)
       })
     end
 
-    if hunk.first == hunk.last then
-      mark(hunk.first, "── " .. hunk.author)
+    if hunk.line_count == 1 then
+      mark(buffer_line, "── " .. hunk.author)
     else
-      mark(hunk.first, "╭─ " .. hunk.author)
-
-      for line = hunk.first + 1, hunk.last - 1 do
-        mark(line, "│")
+      mark(buffer_line, "╭─ " .. hunk.author)
+      for k = 1, hunk.line_count - 2 do
+        mark(buffer_line + k, "│")
       end
-
-      mark(hunk.last, "╰─ ")
+      mark(buffer_line + hunk.line_count - 1, "╰─ ")
     end
+
+    buffer_line = buffer_line + hunk.line_count
+    i = new_i
   end
 
   blame_enabled = true
 end
 
-local hide_blame_view = function(bufnr)
-  if blame_enabled then
+local disable_blame = function(bufnr)
+  if not blame_enabled then
+    vim.notify("Blame hunks already disabled", vim.log.levels.INFO)
+  end
     vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
     blame_enabled = false
-  end
 end
 
 local toggle_blame = function()
   if blame_enabled then
-    hide_blame_view(vim.api.nvim_get_current_buf())
+    disable_blame(vim.api.nvim_get_current_buf())
   else
-    show_blame_view(vim.api.nvim_get_current_buf())
+    enable_blame(vim.api.nvim_get_current_buf())
   end
 
   gitsigns.toggle_current_line_blame()
 end
+
+local blame_is_enabled = function()
+  return blame_enabled
+end
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+  callback = function()
+    if blame_is_enabled() then
+      toggle_blame()
+    end
+  end,
+})
 
 return {
   next_hunk = gitsigns.next_hunk,
@@ -160,4 +158,5 @@ return {
   stage_hunk = gitsigns.stage_hunk,
   reset_hunk = gitsigns.reset_hunk,
   toggle_blame = toggle_blame,
+  blame_is_enabled = blame_is_enabled,
 }
